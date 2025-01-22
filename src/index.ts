e60d6206b2b45ec39240a80462af0bdbb7b2d938
bomsiwor <bomsiwor@gmail.com>
2025-01-22 16:20:08 +0700

    project : start project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,76 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from '@elysiajs/swagger'
+import { PrismaClient } from "@prisma/client";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// Setup prisma
+const prisma = new PrismaClient({
+  log: [
+    'info'
+  ]
+});
+
+const app = new Elysia();
+
+// Add route
+app
+  .use(swagger())
+  .onTransform(({ path, request: { method }, body, params }) => {
+    console.log(`${path} | ${method}`, {
+      body,
+      params,
+    })
+  })
+  .get("/", () => {
+    return { message: "Hello Universe" }
+  })
+  .get("/blog/:slug/comments", async ({ params: { slug } }) => {
+    // Get all blog comments
+    const comments = await prisma.blogComment.findMany({
+      where: {
+        blogSlug: slug
+      },
+      orderBy: {
+        id: "desc"
+      }
+    })
+
+    return {
+      data: comments
+    }
+  },
+    {
+      params: t.Object({
+        slug: t.String()
+      })
+    })
+  .post("/blog/:slug/comments", async ({ body, params: { slug }, set }) => {
+    // Create blog comment
+    const comment = await prisma.blogComment.create({
+      data: {
+        ...body,
+        blogSlug: slug,
+      },
+    })
+
+    // Set status
+    set.status = 201;
+
+    return {
+      data: comment
+    }
+  },
+    {
+      body: t.Object({
+        name: t.String(),
+        body: t.String()
+      })
+    }
+  );
+
+
+// Listen to port
+app.listen(3000)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Bomsiwor BE Portfolio running at ${app.server?.hostname}:${app.server?.port}`
 );
